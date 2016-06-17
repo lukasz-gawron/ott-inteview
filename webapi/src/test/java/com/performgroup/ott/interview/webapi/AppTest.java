fa69b1d0f33cc73cef63cc5aad3685fc0950951f
lukasz.gawron <lukasz.gawron@performgroup.com>
2016-06-17 13:28:34 +0200

    initial commit with webapi project and parent structure

---
+++ webapi/src/test/java/com/performgroup/ott/interview/webapi/AppTest.java
@@ -0,0 +1,18 @@
+package com.performgroup.ott.interview.webapi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest {
+
+    @Test
+    public void should_work() throws Exception {
+
+        assertTrue(true);
+    }
+}
